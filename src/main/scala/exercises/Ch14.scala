48aee3fb013df72ee69b204533c7d3d313066fe5
sitetester <sitetester2010@gmail.com>
2020-02-14 16:27:28 +0200

    Ch14-exercise3 (Using pattern matching, write a function swap that swaps the first two elements of an array provided its length is at least two.)

--- src/main/scala/exercises/Ch14.scala
+++ src/main/scala/exercises/Ch14.scala
@@ -7,4 +7,21 @@ object Ch14 {
       case n1 => (n2, n1)
     }
   }
+
+  def exercise3_swap(a: Array[Int]): Array[Int] = {
+    a match {
+      case a if a.length >= 2 =>
+        val s = a.slice(2, a.length)
+
+        val swapped = Array(a(1), a(0))
+        if (s.length > 0) {
+          val c = swapped ++ s
+          return c
+        }
+
+        swapped
+
+      case _ => a
+    }
+  }
 }
--- src/test/scala/Ch14.scala
+++ src/test/scala/Ch14.scala
@@ -8,4 +8,19 @@ class Ch14 extends AnyFlatSpec {
     assert(Ch14.exercise2_swap(10, 5) == (5, 10))
   }
 
+  it should "exercise3_swap" in {
+
+    // returns REVERSED array for length >=2
+    assert(Ch14.exercise3_swap(Array(1, 2)) sameElements Array(2, 1))
+
+    assert(Ch14.exercise3_swap(Array(1, 2, 3)) sameElements Array(2, 1, 3))
+
+    assert(
+      Ch14.exercise3_swap(Array(1, 2, 3, 4)) sameElements Array(2, 1, 3, 4)
+    )
+
+    // returns SAME array otherwise
+    assert(Ch14.exercise3_swap(Array(1)) sameElements Array(1))
+  }
+
 }
