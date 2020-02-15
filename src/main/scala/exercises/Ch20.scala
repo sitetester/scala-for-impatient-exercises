8e9a1bb5c78c4526f94c8dc6ce1f4ce612218bae
sitetester <sitetester2010@gmail.com>
2020-02-15 22:43:41 +0200

    exercise#3 (Write a parser that parses a list of integers (such as (1, 23, -79)) into a List[Int])

--- src/main/scala/exercises/Ch20.scala
+++ src/main/scala/exercises/Ch20.scala
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter
 
 object Ch20 {
 
+  def exercise3(ints: String): List[Int] = ints.split(", ").map(_.toInt).toList
+
   def exercise4(date: String): LocalDateTime = {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     LocalDateTime.parse(date, formatter)
--- src/test/scala/Ch20.scala
+++ src/test/scala/Ch20.scala
@@ -5,7 +5,11 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class Ch20 extends AnyFlatSpec {
 
-  it should "exercise8" in {
+  it should "exercise3" in {
+    assert(Ch20.exercise3("1, 23, -79") == List(1, 23, -79))
+  }
+
+  it should "exercise4" in {
     val date = "2020-02-15 08:15:30"
     assert(Ch20.exercise4(date).isInstanceOf[LocalDateTime])
   }
