5195a177fb81b2a49256a1a5229a26fd42a27aa0
sitetester <sitetester2010@gmail.com>
2020-02-16 14:07:13 +0200

    exercise#6 (Write a for loop for computing the product of the Unicode codes of all letters in a string. For example, the product of the characters in "Hello" is 9415087488L)

--- src/main/scala/exercises/Ch2.scala
+++ src/main/scala/exercises/Ch2.scala
@@ -5,4 +5,42 @@ object Ch2 {
   def exercise5_countdown(n: Int): Unit = {
     (n to 0 by -1).foreach(println)
   }
+
+  def exercise6(str: String): Unit = {
+
+    // one way
+    val longs = for (c <- str) yield c.toLong
+    println(longs.product)
+
+    // 2nd way
+    println(str.foldLeft(1L)(_ * _.toInt))
+
+    // 3rd way
+    var x = 1L
+    for (c <- str) yield x *= c.toInt
+    println(x)
+
+    // 4rth way
+    var product = 1L
+    for (c <- str) {
+      product *= c.toInt
+    }
+    println(product)
+
+    // 5th way
+    println(str.map(_.toLong).product)
+
+    // 6th way
+    println(str.foldLeft(1L)(_ * _))
+
+    // 7th way
+    println(str.map(_.toLong).reduceLeft(_ * _)) // my IDE warns `Replace reduce with product`
+
+    // 8th way
+    println(str.map(_.toLong).scanLeft(1L)(_ * _).last)
+
+    // 9th way
+    println(str.map(_.toLong).reduce((x, y) => x * y)) // my IDE warns `Replace reduce with product`
+  }
+
 }
--- src/test/scala/Ch2.scala
+++ src/test/scala/Ch2.scala
@@ -5,4 +5,8 @@ class Ch2 extends AnyFlatSpec {
   it should "exercise5" in {
     exercises.Ch2.exercise5_countdown(10)
   }
+
+  it should "exercise6" in {
+    exercises.Ch2.exercise6("Hello")
+  }
 }
