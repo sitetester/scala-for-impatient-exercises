58905bae6b57c809557ca218f9b8b9a77ba0ecf8
sitetester <sitetester2010@gmail.com>
2020-02-17 18:30:16 +0200

    exercise#2 (The preceding problem wasn’t very object-oriented. Provide a general superclass UnitConversion and define objects InchesToCentimeters, GallonsToLiters, and MilesToKilometers that extend it)

--- src/main/scala/exercises/Ch6.scala
+++ src/main/scala/exercises/Ch6.scala
@@ -7,21 +7,46 @@ object Ch6 {
   }
 
   def exercise1_gallonsToLiters(gallons: Double): Double = {
-    Conversions.inchesToCentimeters(gallons)
+    Conversions.gallonsToLiters(gallons)
   }
 
   def exercise1_milesToKilometers(miles: Double): Double = {
     Conversions.milesToKilometers(miles)
   }
 
+  def exercise2_inchesToCentimeters(inches: Double): Double = {
+    (new InchesToCentimeters).convert(inches)
+  }
+
+  def exercise2_gallonsToLiters(inches: Double): Double = {
+    (new GallonsToLiters).convert(inches)
+  }
+
+  def exercise2_milesToKilometers(inches: Double): Double = {
+    (new MilesToKilometers).convert(inches)
+  }
 }
 
 object Conversions {
-
   def inchesToCentimeters(inches: Double): Double = inches * 2.54
 
   def gallonsToLiters(gallons: Double): Double = gallons * 3.7854
 
   def milesToKilometers(miles: Double): Double = miles * 1.60934
+}
 
+abstract class UnitConversion {
+  def convert(unit: Double): Double
+}
+
+class InchesToCentimeters extends UnitConversion {
+  override def convert(unit: Double): Double = unit * 2.54
+}
+
+class GallonsToLiters extends UnitConversion {
+  override def convert(unit: Double): Double = unit * 3.7854
+}
+
+class MilesToKilometers extends UnitConversion {
+  override def convert(unit: Double): Double = unit * 1.60934
 }
--- src/test/scala/Ch6.scala
+++ src/test/scala/Ch6.scala
@@ -8,4 +8,10 @@ class Ch6 extends AnyFlatSpec {
     assert(Ch6.exercise1_gallonsToLiters(1) == 3.7854)
     assert(Ch6.exercise1_milesToKilometers(1) == 1.60934)
   }
+
+  it should "exercise2" in {
+    assert(Ch6.exercise2_inchesToCentimeters(1) == 2.54)
+    assert(Ch6.exercise2_gallonsToLiters(1) == 3.7854)
+    assert(Ch6.exercise2_milesToKilometers(1) == 1.60934)
+  }
 }
