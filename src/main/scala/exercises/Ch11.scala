ce4e4a174e4b2a430954199443e5374bf712cc57
sitetester <sitetester2010@gmail.com>
2020-02-18 21:46:14 +0200

    exercise#10 - Modify the PathComponents object of the preceding exercise to instead define an unapplySeq operation that extracts all path segments. For example, for the file /home/cay/readme.txt, you should produce a sequence of three segments: home, cay, and readme.txt

--- src/main/scala/exercises/Ch11.scala
+++ src/main/scala/exercises/Ch11.scala
@@ -5,24 +5,52 @@ import java.nio.file.{Path, Paths}
 object Ch11 {
 
   def exercise9(): Unit = {
-    val pathComponents = PathComponentsExtractor(Paths.get("demo/demo.txt"))
+    val pathComponents = PathDirAndFileExtractor(Paths.get("demo/demo.txt"))
     println("after apply, we have - " + pathComponents)
 
     println("Extracting data using `unapply` method...")
     val (dir, fileName) = (pathComponents.dir, pathComponents.fileName)
     println(dir, fileName)
   }
+
+  def exercise10(): Unit = {
+    val pathComponents = PathComponentsExtractor(
+      Paths.get("/home/cay/readme.txt")
+    )
+
+    println("after apply, we have - " + pathComponents)
+    println("Extracting data using `unapply` method...")
+    pathComponents.foreach(println(_))
+  }
 }
 
-case class PathComponents(var dir: String, fileName: String)
+case class PathDirAndFile(var dir: String, fileName: String)
+
+case class PathComponents(fileName: Seq[String])
+
+object PathDirAndFileExtractor {
+
+  def apply(path: Path): PathDirAndFile = {
+    PathDirAndFile(path.getParent.toString, path.getFileName.toString)
+  }
+
+  def unapply(dirAndFile: PathDirAndFile): Some[(String, String)] = {
+    Some(dirAndFile.dir, dirAndFile.fileName)
+  }
+}
 
 object PathComponentsExtractor {
 
-  def apply(path: Path): PathComponents = {
-    PathComponents(path.getParent.toString, path.getFileName.toString)
+  def apply(path: Path): Seq[String] = {
+    var s = path.toString
+    if (s.startsWith("/")) {
+      s = s.substring(1)
+    }
+
+    s.split("/").toSeq
   }
 
-  def unapply(pathComponents: PathComponents): Some[(String, String)] = {
-    Some(pathComponents.dir, pathComponents.fileName)
+  def unapply(pathComponents: PathComponents): Some[Seq[String]] = {
+    Some(pathComponents.fileName)
   }
 }
--- src/test/scala/Ch11.scala
+++ src/test/scala/Ch11.scala
@@ -6,4 +6,8 @@ class Ch11 extends AnyFlatSpec {
   it should "exercise9" in {
     Ch11.exercise9()
   }
+
+  it should "exercise10" in {
+    Ch11.exercise10()
+  }
 }
